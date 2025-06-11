17bca646258c7d3b71f7bf0872ffc04808bcc963
Keylan King <kingkey0101@outlook.com>
2025-06-11 14:42:52 -0500

    added CRUD operations, still need to fix some bugs

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
   doc,
   query,
   where,
+  updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import {
   createUserWithEmailAndPassword,
@@ -21,16 +23,58 @@ import Nav from "./firebase/Nav";
 //register user
 function App() {
   const [user, setUser] = React.useState({});
+  //loading state while fetching user info
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setTimeout(() => {
+        setUser(firebaseUser || {});
+        setLoading(false);
+      }, 300);
+      //print first letter of email
+      console.log(firebaseUser);
+
+      // if (user) {
+      //   setUser(user);
+      // } else {
+      //   setLoading(true);
+      // }
+    });
+    return unsubscribe;
+  }, []);
 
   //adding post to DataBase(adding document)
   function createPost() {
     const post = {
       title: "Finish Firebase",
       description: "Do Frontend Simplified",
-      uid: user.id,
+      uid: user.uid,
     };
-   
-    addDoc(collection(db, "posts"), post,);
+
+    addDoc(collection(db, "posts"), post);
+  }
+
+  //updating post
+  async function updatePost() {
+    const hardcodedID = "JoL0rG4kudt1Ya266qZ5";
+    const postRef = doc(db, "posts", hardcodedID);
+    const post = await getPostById(hardcodedID);
+    console.log(post);
+    const newPost = {
+      //only changing title of post
+      //using all data just feched by (post) using spread operator
+      ...post,
+      title: "land $700k job",
+    };
+    console.log(newPost);
+    updateDoc(postRef, newPost);
+  }
+
+  function deletePost() {
+       const hardcodedID = "JoL0rG4kudt1Ya266qZ5";
+    const postRef = doc(db, "posts", hardcodedID);
+    deleteDoc(postRef)
   }
 
   //Read all posts(shown on screen)
@@ -43,25 +87,37 @@ function App() {
   }
 
   //get post by id
-  async function getPostById() {
+  async function getPostById(id) {
     const hardcodedID = "JoL0rG4kudt1Ya266qZ5";
-
-    const postRef = doc(db, "posts", hardcodedID);
+    const postRef = doc(db, "posts", id);
     const postSnap = await getDoc(postRef);
 
     //only show if exists
     if (postSnap.exists()) {
-      const post = postSnap.data();
-      console.log(post);
+      return postSnap.data();
     }
+  }
+
+  //read posts by query(will only work if logged in)
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    //gives query post info in JavaScript
+    console.log(docs.map((doc) => doc.data()));
   }
 
   return (
     <div className="App">
-      <Nav />
+      <Nav user={user} setUser={setUser} loading={loading} />
       <button onClick={createPost}>Create Post</button>
       <button onClick={getAllPosts}>Get All Posts</button>
       <button onClick={getPostById}>Get Posts By Id</button>
+      <button onClick={getPostByUid}>Get Posts By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
--- src/firebase/Nav.jsx
+++ src/firebase/Nav.jsx
@@ -10,99 +10,68 @@ import {
 } from "firebase/auth";
 import logo from "../assets/kk logo2.png";
 
-const Nav = () => {
-  const [user, setUser] = React.useState({});
-
-  //loading state while fetching user
-  const [loading, setLoading] = React.useState(true);
-
-  //run empty array when page mounts []
-  //signs user back in on page reload
-  React.useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      //showing loading state
-      setTimeout(() => {
-        setUser(user || {});
-        setLoading(false);
-      }, 300);
-
-      //print first letter of email
-      console.log(user);
-
-      if (user) {
-        setUser(user);
-      } else {
-        setLoading(true);
-      }
-    });
-  }, []);
-
+const Nav = ({ user, setUser, loading }) => {
   //register user
   function register() {
-    setLoading(true);
     console.log("register");
     createUserWithEmailAndPassword(auth, "email@email.com", "password123")
-      .then((user) => {
-        setLoading(false);
+      .then((userCredential) => {
+        setUser(userCredential.user);
         console.log(user);
       })
       .catch((error) => {
-        setLoading(false);
         console.log(error);
       });
   }
 
   //login user
   function login() {
-    setLoading(true);
     signInWithEmailAndPassword(auth, "email@email.com", "password123")
       .then(({ user }) => {
         setUser(user);
-        setLoading(false);
         console.log(user);
       })
       .catch((error) => {
-        setLoading(false);
         console.log(error.message);
       });
   }
 
   //logout user
   function logout() {
-    setLoading(true);
-    signOut(auth);
-    setUser({});
-    setLoading(false);
-    console.log("signedout");
+    signOut(auth)
+    .then(() => {
+      setUser({});
+      console.log("signedout");
+    });
   }
 
   return (
     <>
-    <div className="nav__bar">
-      <div className="nav__logo--wrapper">
-        <img src={logo} alt="" className="nav__logo" />
-      </div>
-      <div className="nav__links">
-        {loading ? (
-          <div className="nav__skeleton">Loading...</div>
-        ) : user && user.email ? (
-          <div className="nav__link--logout">
-            <button className="btn__logout" onClick={logout}>
-              {loading ? "" : user.email[0].toUpperCase()}
-            </button>
-          </div>
-        ) : (
-          <>
-            <div className="nav__link--login">
-              <button onClick={login}>Login</button>
+      <div className="nav__bar">
+        <div className="nav__logo--wrapper">
+          <img src={logo} alt="" className="nav__logo" />
+        </div>
+        <div className="nav__links">
+          {loading ? (
+            <div className="nav__skeleton">Loading...</div>
+          ) : user && user.email ? (
+            <div className="nav__link--logout">
+              <button className="btn__logout" onClick={logout}>
+                {loading ? "" : user.email[0].toUpperCase()}
+              </button>
             </div>
-            <div className="nav__link--register">
-              <button onClick={register}>Register</button>
-            </div>
-          </>
-        )}
+          ) : (
+            <>
+              <div className="nav__link--login">
+                <button onClick={login}>Login</button>
+              </div>
+              <div className="nav__link--register">
+                <button onClick={register}>Register</button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
