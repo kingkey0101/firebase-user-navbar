247e6d28edba134b7bc7fe06d9c0254905b32294
Keylan King <kingkey0101@outlook.com>
2025-06-10 23:01:46 -0500

    adding firestore db and get post data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { auth } from "./firebase/init";
+import { auth, db } from "./firebase/init";
+import { collection, addDoc, getDocs, getDoc, doc } from "firebase/firestore";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -11,10 +12,44 @@ import Nav from "./firebase/Nav";
 
 //register user
 function App() {
+  //adding post to DataBase(adding document)
+  function createPost() {
+    const post = {
+      title: "Land a $400k job",
+      description: "Finish Frontend Simplified",
+    };
+    addDoc(collection(db, "posts"), post);
+  }
+
+  //Read all posts(shown on screen)
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+
+    //turns it to JavaScript
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts);
+  }
+
+  //get post by id
+  async function getPostById() {
+    const hardcodedID = "JoL0rG4kudt1Ya266qZ5";
+
+    const postRef = doc(db, "posts", hardcodedID);
+    const postSnap = await getDoc(postRef);
+    
+    //only show if exists
+    if (postSnap.exists()) {
+      const post = postSnap.data();
+      console.log(post);
+    }
+  }
 
   return (
     <div className="App">
       <Nav />
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Posts By Id</button>
     </div>
   );
 }
---
+++ src/firebase/init.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ19K-TSLQCVl4syTUqZ9HGr9FKm8Auzs",
+  authDomain: "fir-1-3f13b.firebaseapp.com",
+  projectId: "fir-1-3f13b",
+  storageBucket: "fir-1-3f13b.firebasestorage.app",
+  messagingSenderId: "975267565230",
+  appId: "1:975267565230:web:69a8c0309a95ba539e673f",
+  measurementId: "G-JE7940B6F4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore();
+const analytics = getAnalytics(app);
